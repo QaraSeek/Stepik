d486ffd220db08acb41bcf85f3f899a146aa10ae
QaraSeek <1allekc1@gmail.com>
2021-01-31 17:47:01 +0300

    Типичный hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,5 @@
 public class HelloWorld {
     public static void main (String[] args){
-        int a;
-        int b;
-        int c;
-        a = 0;
-
-        if (a==0 | 100/a > 1){
-            System.out.println("COCK");
-        }else{
-            System.out.println("PIPISKA");
-        }
-
-
-
         System.out.println("Hello world");
     }
 }
